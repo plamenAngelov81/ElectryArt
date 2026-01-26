77280608a22444148d7f7ae1e720b252be70f9a2
Пламен Ангелов <103038521+plamenAngelov81@users.noreply.github.com>
2026-01-27 00:44:01 +0200

    Update settings for secret key and database config
    Updated the SECRET_KEY and database credentials for PostgreSQL.

--- electry_art/settings.py
+++ electry_art/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#'
+SECRET_KEY = 'django-insecure-d!xyn4lfu4fj@qjzs6k0#jqu6*xje648_%nx(#f15u5t!=6fi0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,8 +88,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'electry_art_db',
-        'USER': '#######',
-        'PASSWORD': '######',
+        'USER': 'postgres',
+        'PASSWORD': 'Buldozer4',
         'HOST': 'localhost',
         'PORT': '5432',
     }
